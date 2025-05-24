220e2547c30110eabd9e2b1a8644b089ce5e0f3a
coderkeshav-yt <coderkeshavyt@gmail.com>
2025-05-24 12:04:55 +0530

    Web Analytics vercel

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
 import { CartProvider } from './contexts/CartContext'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AuthProvider>
       <CartProvider>
         <App />
+        <Analytics />
       </CartProvider>
     </AuthProvider>
   </BrowserRouter>
